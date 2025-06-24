12c9d5741b2396488543ae985d6ab4c46d0bebb0
tylerengineer <tyler.engineer@hotmail.com>
2025-06-24 14:25:51 -0400

    Created colors.lua file

---
+++ lua/inkwell/colors.lua
@@ -0,0 +1,26 @@
+-- lua/inkwell/colors.lua
+
+return {
+    light = {
+        bg = '#eae0d5',
+        fg = '#22333b',
+        linen = '#eae0d5',
+        tan = '#c6ac8f',
+        bark = '#5e503f',
+        ink = '#22333b',
+        shadow = '#0a0908',
+        jade = '#02c39a',
+        mint = '#1dd3b0',
+    },
+    dark = {
+        bg = '#22333b',
+        fg = '#eae0d5',
+        linen = '#eae0d5',
+        tan = '#c6ac8f',
+        bark = '#5e503f',
+        ink = '#1a1e21',
+        shadow = '#0a0908',
+        jade = '#02c39a',
+        mint = '#1dd3b0',
+    },
+}
