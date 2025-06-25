cfd40dd83ac1f895679913acce1dcaa836167f53
tylerengineer <tyler.engineer@hotmail.com>
2025-06-25 17:43:55 -0400

    Updated init.lua (inner)

--- lua/inkwell/init.lua
+++ lua/inkwell/init.lua
@@ -1,11 +1,14 @@
 -- lua/inkwell/init.lua
-
 local M = {}
 
-M.load = function(variant)
+M.load = function(opts)
+    opts = opts or {}
+    local variant = opts.variant or 'dark'
+    local transparent = opts.transparent or false
+
     local colors = require('inkwell.colors')
     local theme = require('inkwell.' .. variant)
-    theme.setup(colors[variant])
+    theme.setup(colors[variant], transparent)
 end
 
 return M
