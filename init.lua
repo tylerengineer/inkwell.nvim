35ac8c69613164dbe641ad13be6e5d79f32b9593
tylerengineer <tyler.engineer@hotmail.com>
2025-06-25 17:44:07 -0400

    Updated root init.lua (outer)

--- init.lua
+++ init.lua
@@ -1,4 +1,18 @@
 -- inkwell.nvim/init.lua
 -- https://github.com/tylerengineer/inkwell.nvim
 
-require('inkwell').load('dark') -- or "light"
+require('inkwell').load({ variant = 'dark', transparent = false })
+
+-- local M = {}
+--
+-- M.load = function(opts)
+--     opts = opts or {}
+--     local variant = opts.variant or 'dark'
+--     local transparent = opts.transparent or false
+--
+--     local colors = require('inkwell.colors')
+--     local theme = require('inkwell.' .. variant)
+--     theme.setup(colors[variant], transparent)
+-- end
+--
+-- return M
